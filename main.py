1338f445aac656a7f470e6cb15f67e78cb8180a3
Gian <you@example.com>
2024-05-10 16:25:59 -0300

    include abort to send error responses

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__) #our main object
 api = Api(app) #main entry point of our application
@@ -10,16 +10,28 @@ video_put_args.add_argument("likes", type=int, help="Number of likes of the vide
 video_put_args.add_argument("views", type=int, help="Views of the video")
 videos = {}
 
+def video_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video Id " + str(video_id) + " does not exist.") #sends http error message given a status code and message
+
+def video_already_exists(video_id):
+    if video_id in videos:
+        abort(400,message="Video Id " + str(video_id) + " already exists.")
+
+
 class Video(Resource): #A class that inherits a resource. Allows us to override methods from the Resource such as GET, PUT, POST, etc. 
     def get(self, video_id):
+        video_doesnt_exist(video_id)
         return videos.get(video_id) #Using dictionary to make it JSON serializable as JSON has a key value pair format
     
     def post(self, video_id):
+        video_already_exists(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
-        return "Put Successful"
+        return videos[video_id], 201
     
     def delete(self, video_id):
+        video_doesnt_exist(video_id)
         videos.pop(video_id)
         return "Delete Successful"
     
--- test.py
+++ test.py
@@ -5,6 +5,12 @@ BASE = "http://127.0.0.1:5000"
 response = requests.post(BASE + "/video/1", {"name": "Pog Video", "views": 10000, "likes": 10})
 print(response.json())
 
+response = requests.post(BASE + "/video/2", {"name": "Basketball Video", "views": 10000000, "likes": 1000})
+print(response.json())
+
+response = requests.post(BASE + "/video/3", {"name": "Boxing Video", "views": 200, "likes": 32})
+print(response.json())
+
 response = requests.get(BASE + "/video/1")
 print(response.json())
 
@@ -18,6 +24,9 @@ response = requests.delete(BASE + "/video/1")
 print(response.json())
 
 response = requests.get(BASE + "/video/1")
+print(response.json())
+
+response = requests.post(BASE + "/video/3", {"name": "Boxing Video", "views": 200, "likes": 32})
 print(response.json())
 
 
